0964b40625f545b3982762e70e566bbcd5aa104b
Manish Elaganti <65448066+Manishreddy619@users.noreply.github.com>
2021-08-09 16:10:14 +0200

    final commit

--- script.js
+++ script.js
@@ -346,3 +346,291 @@ const acronym = (str = '') => {
 };
 console.log(acronym(str));
 console.log(acronym('Bachelor of Science'));
+
+// // 1) MAX CHAR
+
+// //  Given a string, return the character that is most
+//  commonly used in the string.
+const getMax = function (str) {
+	let max = 0,
+		maxChar = '';
+	str.split('').forEach(function (char) {
+		if (str.split(char).length > max) {
+			max = str.split(char).length;
+			maxChar = char;
+		}
+	});
+	return maxChar;
+};
+console.log(getMax('wuvgef9ig0000000000000000'));
+// // --- Examples
+// //     maxChar("abcccccccd") === "c"
+// //     maxChar("apple 1231111") === "1"
+
+// /* 2) ANAGRAMS
+
+// // Check to see if two provided strings are anagrams of each other.
+// // One string is an anagram of another if it uses the same characters
+// // in the same quantity. Only consider characters, not spaces
+// // or punctuation.  Consider capital letters to be the same as lower case
+function areAnagram(a, b) {
+	let len1 = a.length;
+	let len2 = b.length;
+	if (len1 !== len2) {
+		console.log('Invalid Input');
+		return;
+	}
+	let str1 = a.split('').sort().join('');
+	let str2 = b.split('').sort().join('');
+	if (str1.toLowerCase() === str2.toLowerCase()) {
+		return true;
+	} else {
+		return false;
+	}
+}
+console.log(areAnagram('indian', 'Ndiani'));
+console.log(areAnagram('listen', 'nEtsil'));
+
+// // --- Examples
+// //   anagrams('rail safety', 'fairy tales') --> True
+// //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+// //   anagrams('Hi there', 'Bye there') --> False
+// */
+
+// /* 3) ANAGRAMS 2
+
+// Given a word and a list of possible anagrams, select the correct sublist.
+
+// --- Examples
+
+//     "listen" and a list of candidates like "enlists" "google" "inlets" "banana" the program should return a list containing "inlets".
+// */
+
+// /* 4) PALINDROME
+
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. Do include spaces
+// and punctuation in determining if the string is a palindrome.
+function palindrome(str) {
+	str = str.toLowerCase(); //Turn everything to lower case
+	// Remove all alphanumeric characters.
+	str = str.replace(/[^a-z0-9]/g, '');
+	let backward = str.split('');
+	backward = backward.reverse().join('').toString();
+
+	if (backward === str) {
+		return true;
+	} else {
+		return false;
+	}
+}
+console.log('**************************************');
+console.log(palindrome('eye'));
+
+// --- Examples:
+
+//     palindrome("abba") === true
+//     palindrome("abcdefg") === false
+//  */
+
+// /* 5) REVERSE INT
+
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+function reverseInt(n) {
+	const reversed = n.toString().split('').reverse().join(''); // turn a number into a string, then turn it into an array to reverse.
+	return Math.sign(n) * parseInt(reversed); // Math.sign will return -1 as for negative number, 1 as for position number, 0 as for zero.
+}
+console.log(reverseInt(6554));
+// --- Examples
+
+//     reverseInt(15) === 51
+//     reverseInt(981) === 189
+//     reverseInt(500) === 5
+//     reverseInt(-15) === -51
+//     reverseInt(-90) === -9
+//  */
+
+// /* 6) STEPS
+
+// Write a function that accepts a positive number N.
+// The function should console log a step shape
+// with N levels using the # character.  Make sure the
+// step has spaces on the right hand side!
+
+// --- Examples
+
+//     steps(2)
+//         '# '
+//         '##'
+//     steps(3)
+//         '#  '
+//         '## '
+//         '###'
+//     steps(4)
+//         '#   '
+//         '##  '
+//         '### '
+//         '####' */
+function steps(n, i = 1) {
+	if (i > n) return;
+	console.log('#'.repeat(i) + ' '.repeat(n - i));
+	steps(n, i + 1);
+}
+
+console.log(steps(4));
+// /* 7) REVERSE STRING
+
+// Given a string, return a new string with the reversed
+// order of characters
+
+// --- Examples
+
+//     reverse('apple') === 'leppa'
+//     reverse('hello') === 'olleh'
+//     reverse('Greetings!') === '!sgniteerG'
+//  */
+function reversed(str) {
+	str = str.toLowerCase(); //Turn everything to lower case
+	// Remove all alphanumeric characters.
+	str = str.replace(/[^a-z0-9]/g, '');
+	let backward = str.split('');
+	backward = backward.reverse().join('').toString();
+	return backward;
+}
+console.log(reversed('apple'));
+console.log(reversed('Greetings!'));
+// /* 8) CHUNK
+
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+
+// --- Examples
+
+//     chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+//     chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+//     chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+//     chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+//     chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+// */
+function chunk(array, size) {
+	const chunked_arr = [];
+	for (let i = 0; i < array.length; i++) {
+		const last = chunked_arr[chunked_arr.length - 1];
+		if (!last || last.length === size) {
+			chunked_arr.push([array[i]]);
+		} else {
+			last.push(array[i]);
+		}
+	}
+	return chunked_arr;
+}
+
+console.log(chunk([1, 2, 3, 4], 2));
+// /* 9) PYRAMID
+
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left and right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+function pyramid(n, row = 0, level = '') {
+	if (row === n) return;
+	if (level.length === 2 * n - 1) {
+		console.log(level);
+		return pyramid(n, row + 1);
+	}
+	const midpoint = Math.floor((2 * n - 1) / 2);
+	let add;
+	if (midpoint - row <= level.length && midpoint + row >= level.length) {
+		add = '#';
+	} else {
+		add = ' ';
+	}
+	pyramid(n, row, level + add);
+}
+console.log(pyramid(5));
+
+// --- Examples
+
+//     pyramid(1)
+//         '#'
+//     pyramid(2)
+//         ' # '
+//         '###'
+//     pyramid(3)
+//         '  #  '
+//         ' ### '
+//         '#####' */
+
+// /* 10) SPYRAL MATRIX
+
+// // Write a function that accepts an integer N
+//  and returns a NxN spiral matrix.
+function matrix(n) {
+	const results = [];
+
+	for (let i = 0; i < n; i++) {
+		results.push([]);
+	}
+
+	let counter = 1;
+
+	let startColumn = 0;
+	let endColumn = n - 1;
+	let startRow = 0;
+	let endRow = n - 1;
+
+	while (startColumn <= endColumn && startRow <= endRow) {
+		for (let i = startColumn; i <= endColumn; i++) {
+			results[startRow][i] = counter;
+			counter++;
+		}
+		startRow++;
+
+		for (let i = startRow; i <= endRow; i++) {
+			results[i][endColumn] = counter;
+			counter++;
+		}
+		endColumn--;
+
+		for (let i = endColumn; i >= startColumn; i--) {
+			results[endRow][i] = counter;
+			counter++;
+		}
+		endRow--;
+
+		for (let i = endRow; i >= startRow; i--) {
+			results[i][startColumn] = counter;
+			counter++;
+		}
+		startColumn++;
+	}
+	return results;
+}
+console.log(matrix(5));
+// --- Examples
+
+//     matrix(2)
+//         [[1, 2],
+//         [4, 3]]
+//     matrix(3)
+//         [[1, 2, 3],
+//         [8, 9, 4],
+//         [7, 6, 5]]
+//     matrix(4)
+//         [[1, 2, 3, 4],
+//         [12, 13, 14, 5],
+//         [11, 16, 15, 6],
+//         [10,  9,  8, 7]]
